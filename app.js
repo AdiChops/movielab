d89eade3c0a1a275c48b527376eac8cc29722bc7
AdiChops <Chopra.Aaditya@cegep-heritage.qc.ca>
2021-03-07 19:26:18 -0500

    added functionality to pages

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const pug = require('pug');
+const currentUser = require('./data/user.json')
 const app = express();
 const port = 3000;
+app.use(express.static('public'));
 
-app.get('/', (req,res)=>{
-    res.send('Hello World!');
+app.get('/login', (req,res)=>{
+    res.sendFile('login.html', {root: './public'});
+});
+
+app.get(['/', '/index'], (req,res)=>{
+    res.send(pug.renderFile('./templates/profileTemplate.pug', {currentUser}));
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
 });
 
 app.listen(port, ()=>{
