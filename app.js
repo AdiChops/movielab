f2036c965a0fea590801de594ce5cc7a020c0bd8
AdiChops <Chopra.Aaditya@cegep-heritage.qc.ca>
2021-03-08 00:47:48 -0500

    changes made to console message

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ app.use(function (req, res, next) {
 });
 
 app.listen(port, ()=>{
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Server listening at http://localhost:${port}`);
 });
